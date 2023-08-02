9b08140c0df25ae0f5084773a23ee3ececb215a0
farid-moradi <f.moradi.d@gmail.com>
2023-08-02 19:00:37 +0330

    changing everything to model graphs

--- dijkstra/dijkstra.go
+++ dijkstra/dijkstra.go
@@ -1,48 +1,118 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type graph struct {
-	gr map[*node][]edge
-	v  int
-	e  int
+	nodes map[*node]map[*node]*edge
 }
 
 func NewGraph() *graph {
-	g := make(map[*node][]edge)
-	return &graph{g, 0, 0}
+	grph := make(map[*node]map[*node]*edge)
+	return &graph{grph}
 }
 
 func (g *graph) AddNode(v *node) {
-	g.gr[v] = make([]edge, 0)
+	g.nodes[v] = make(map[*node]*edge)
 }
 
-func (g *graph) AddEdge(v *node, e *edge) {
-	g.gr[v] = append(g.gr[v], *e)
+func (g *graph) AddEdge(p *node, v *node, e *edge) {
+	g.nodes[p][v] = e
 }
 
 func (g *graph) Print() {
-	for v, e := range g.gr {
-		for _, edges := range e {
-			fmt.Printf("(src: %s, dst: %s, weigth: %d)\n", v.name, edges.dst.name, edges.w)
+	for i := range g.nodes {
+		for j := range g.nodes[i] {
+			fmt.Printf("(src: %s, dst: %s, weigth: %d)\n", i.name, j.name, g.nodes[i][j].w)
 		}
 	}
+
 }
 
-func (g *graph) PrintNeighbor(n *node) {
-	for _, edges := range g.gr[n] {
-		fmt.Println(edges.dst.name)
+// func (g *graph) PrintNeighbor(n *node) {
+// 	for _, edges := range g.gr[n] {
+// 		fmt.Println(edges.dst.name)
+// 	}
+// }
+
+func (g *graph) Dijkstra(start *node) *[]node {
+	costs := make(map[*node]float64)
+	parents := make(map[*node]*node)
+	infinity := math.Inf(1)
+
+	for v := range g.nodes {
+		if _, ok := g.nodes[start][v]; ok {
+			parents[v] = start
+			costs[v] = float64(g.nodes[start][v].w)
+		} else {
+			parents[v] = nil
+			costs[v] = infinity
+		}
 	}
+
+	var processed []*node
+	nodeCheck := findLowestCostNode(costs, processed)
+	for nodeCheck != nil {
+		cost := costs[nodeCheck]
+		neighbors := g.nodes[nodeCheck]
+		for n := range neighbors {
+			newCost := cost + float64(g.nodes[nodeCheck][n].w)
+			if costs[n] > newCost {
+				costs[n] = newCost
+				parents[n] = nodeCheck
+			}
+		}
+		processed = append(processed, nodeCheck)
+		nodeCheck = findLowestCostNode(costs, processed)
+	}
+	fmt.Println("costs:")
+	for i, v := range costs {
+		fmt.Println(i.name, v)
+	}
+	fmt.Println("parents:")
+	for i, v := range parents {
+		if v != nil {
+			fmt.Println(i.name, v.name)
+		}
+	}
+	// TODO
+	return nil
 }
 
-// func (g *graph) Dijkstra(start *node) *[]node {
-// 	fmt.Printf("%v\n", g.v)
-// 	return nil
+func findLowestCostNode(costs map[*node]float64, processed []*node) *node {
+	lowestCost := math.Inf(1)
+	var lowestCostNode *node
+	for node := range costs {
+		cost := costs[node]
+		found := false
+		for _, n := range processed {
+			if n == node {
+				found = true
+			}
+		}
+		if cost < lowestCost && !found {
+			lowestCost = cost
+			lowestCostNode = node
+		}
+	}
+	return lowestCostNode
+}
+
+// func (g *graph) Weigth(p *node, n *node) (uint, error) {
+// 	for _, e := range g.gr[p] {
+// 		if e.dst == n {
+// 			return e.w, nil
+// 		}
+// 	}
+// 	err := errors.New("not an edge")
+// 	return 0, err
 // }
 
 // this prints every single path from node n in a DAG
 func (g *graph) PrintAllThePathsNode(n *node, path []*node) {
-	if len(g.gr[n]) == 0 {
+	if len(g.nodes[n]) == 0 {
 		if len(path) != 0 {
 			for _, i := range path {
 				fmt.Printf("%s ", i.name)
@@ -52,10 +122,19 @@ func (g *graph) PrintAllThePathsNode(n *node, path []*node) {
 		return
 	}
 	path = append(path, n)
-	for _, edges := range g.gr[n] {
-		g.PrintAllThePathsNode(edges.dst, path)
+	for nei := range g.nodes[n] {
+		g.PrintAllThePathsNode(nei, path)
 	}
 }
+
+// func (g *graph) IsParent(n *node, p *node) bool {
+// 	for _, t := range g.parents[n] {
+// 		if t == p {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
 
 type node struct {
 	name string
@@ -66,12 +145,11 @@ func NewNode(name string) *node {
 }
 
 type edge struct {
-	w   uint
-	dst *node
+	w uint
 }
 
-func NewEdge(w uint, dst *node) *edge {
-	return &edge{w, dst}
+func NewEdge(w uint) *edge {
+	return &edge{w}
 }
 
 func main() {
@@ -80,11 +158,11 @@ func main() {
 	b := NewNode("B")
 	fin := NewNode("FIN")
 
-	e1 := NewEdge(6, a)
-	e2 := NewEdge(1, fin)
-	e3 := NewEdge(3, a)
-	e4 := NewEdge(2, b)
-	e5 := NewEdge(5, fin)
+	e1 := NewEdge(6)
+	e2 := NewEdge(1)
+	e3 := NewEdge(3)
+	e4 := NewEdge(2)
+	e5 := NewEdge(5)
 
 	g := NewGraph()
 	g.AddNode(start)
@@ -92,12 +170,14 @@ func main() {
 	g.AddNode(b)
 	g.AddNode(fin)
 
-	g.AddEdge(start, e1)
-	g.AddEdge(a, e2)
-	g.AddEdge(b, e3)
-	g.AddEdge(start, e4)
-	g.AddEdge(b, e5)
+	g.AddEdge(start, a, e1)
+	g.AddEdge(a, fin, e2)
+	g.AddEdge(b, a, e3)
+	g.AddEdge(start, b, e4)
+	g.AddEdge(b, fin, e5)
 
-	g.PrintAllThePathsNode(b, nil)
+	// g.Print()
+	// g.PrintAllThePathsNode(start, nil)
+	g.Dijkstra(start)
 
 }
